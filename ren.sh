6ef450af6a7e1e65563da3c6a78f7f23537ebd33
BOY122333 <pribadisaya10101010@gmail.com>
2024-05-29 18:34:42 +0700

    Update ren.sh

--- ren.sh
+++ ren.sh
@@ -51,7 +51,7 @@ echo
 echo
 echo -e "                       V̳E̳R̳S̳I̳ T̳E̳R̳B̳A̳R̳U̳ T̳O̳O̳L̳S̳R̳E̳N̳v̳2̳"
 echo -e "                            𝐁𝐲: 𝐑𝐄𝐍𝟗𝟗𝟗𝟗 ☕🗿"
-echo -e "\x1B[32m                      
+echo -e "\x1B[92m                      
                 ||=======================================||
                 ||        WELCOME TO TOOLSRENv2          ||
                 ||=======================================||
